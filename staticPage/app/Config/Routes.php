123f5a6ed3da7cbc07fc189738aa19be8fa5b85e
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-09-07 17:15:21 +0800

    Add files via upload

---
+++ staticPage/app/Config/Routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Controllers\Pages;
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+// Add your Pages controller routes below
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'view']);
